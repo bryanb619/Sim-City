20a910fd0ce82fffd55e8d25367e1560d2fd3f37
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-18 21:38:31 +0100

    Added Agent script

---
+++ Assets/Scripts/AI/Agent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.AI
+{
+    // Automatically add the NavMeshAgent to the GameObject
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Agent : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            
+        }
+    }
+
+}
