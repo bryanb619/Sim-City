319fb89060e1d7997a782663c652731769e6c576
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-25 17:49:39 +0100

    Fix camera movement Movement does not include Rotation Include: X,Y,Z (All Axis)

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
     
     [SerializeField]
-    private float panSpeed = 20f;
+    private float panSpeed = 25f;
 
-    private float _panBorderThickness = 10f;
+    private float panBorderThickness = 20f;
 
     
-
     /// <summary>
     /// Start is called before the first frame update
     /// </summary>
@@ -26,6 +26,7 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         MovInput();
+        //RotateInput();
     }
 
     /// <summary>
@@ -36,17 +37,54 @@ public class CameraController : MonoBehaviour
 
         Vector3 pos = transform.position;
 
+        // -------------------- Vertical Movement --------------------------- //
 
-        if(Input.GetKey("w") || Input.mousePosition.y >= 
-        Screen.height - _panBorderThickness)
-        {
-            pos.z += panSpeed * Time.deltaTime;
-        }
+        if (Input.GetKey("w") || Input.mousePosition.y >= 
+        Screen.height - panBorderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
         }
 
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        // -------------------- Horizontal Movement ------------------------- //
+
+        if (Input.GetKey("d") || Input.mousePosition.x 
+        >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x 
+        <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        // ---------------- UP and DOWN Movement ---------------------------- //
+
+        if (Input.GetKey("q"))
+        {
+            pos.y -= 10 * Time.deltaTime;
+        }
+
+         if (Input.GetKey("e"))
+        {
+            pos.y += 10 * Time.deltaTime;
+        }
 
         transform.position = pos;
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void RotateInput()
+    {
+
     }
 }
