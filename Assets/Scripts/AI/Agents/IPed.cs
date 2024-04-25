f4fcd26890b90590038e108ad088137cffbf5623
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-25 12:14:24 +0100

    Add Interfaces for Car & Ped

---
+++ Assets/Scripts/AI/Agents/IPed.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sim City.Assets.Scripts.AI.Agents
+{
+    public interface IPed
+    {
+        
+    }
+}
