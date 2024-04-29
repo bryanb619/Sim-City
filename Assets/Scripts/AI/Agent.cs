e29b6af259671cc3ffca7ecc62bee6f9180fca0b
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-29 15:14:45 +0100

    Remove unecessary using from Agent

--- Assets/Scripts/AI/Agent.cs
+++ Assets/Scripts/AI/Agent.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Assets.Scripts.AI
 {
     public abstract class Agent
