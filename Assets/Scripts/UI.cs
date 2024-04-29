fc43a9f7491751aee3b2e3cc04a56772b3fd07bf
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-29 18:08:54 +0100

    Add UI logic & XML update consist in adding a button logic, key pressed logic to disable a UI element in game view

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI : MonoBehaviour
+
+namespace Assets.Scripts.AI
 {
-    // Start is called before the first frame update
-    private void Start()
+    public class UI : MonoBehaviour
     {
-        
-    }
+        // Reference to the UI GameObject
 
-    // Update is called once per frame
-    private void Update()
-    {
-        
+        [Tooltip("Ref to UI. Can be null. If Null it will automatically " 
+        +"find the first Image component in the children.")]
+        [SerializeField]    private GameObject      _ui;
+
+        // Key to toggle the UI on/off
+        [Tooltip("Key to toggle the UI on/off")]
+        [SerializeField]    private KeyCode         _key;
+
+        /// <summary>
+        /// Start is called before the first frame update.
+        /// If _ui is null, Start will automatically find the first Image component in the children.
+        /// </summary>
+        private void Start()
+        {
+            if (_ui == null)
+            {
+                _ui =  GetComponentInChildren<Image>().gameObject;
+            }
+        }
+
+
+        /// <summary>
+        /// Update is called once per frame
+        /// Detects if the key is pressed and toggles the UI on/off.
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetKeyDown(_key))
+            {
+                DetectClick();
+            }
+        }
+
+
+        /// <summary>
+        /// Public method can be used as a button or key press to toggle the UI on/off.
+        /// </summary>
+        public void DetectClick()
+        {
+            if (_ui.activeSelf)
+            {
+                _ui.SetActive(false);
+            }
+            else
+            {
+                _ui.SetActive(true);
+            }
+        }
     }
 }
+// -----------------------------------------------------------------------------
