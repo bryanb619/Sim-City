90a87e0d2814ad888c5d8548e2b3aa480932888a
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-30 23:03:27 +0100

    Add TrafficLight.cs States & random state

--- Assets/Scripts/AI/TraficLight.cs
+++ Assets/Scripts/AI/TraficLight.cs
@@ -1,21 +1,68 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LibGameAI.FSMs;
 
+
 public class TraficLight : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     
+
+    private StateMachine _fsm;
+
+    [SerializeField] private enum lightState
+    {
+        yellow, red, green
+    }
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
     private void Start()
+    {
+        System.Random rand = new System.Random(); 
+
+        int i = rand.Next(0, 2);
+
+        State yellowState = new State(  "Yellow", YellowLight,  null, null);
+        State redState = new State(     "Red",    RedLight,     null, null);
+        State greenState = new State(   "Green",  GreenLight,   null, null);
+
+
+        List <State> states = new List<State>
+        {
+            yellowState, redState, greenState
+        };
+
+        _fsm = new StateMachine(states[i]);
+
+
+        // Transitions
+
+        
+    }
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    private void Update()
+
+    private void YellowLight()
     {
-        
+
+    }
+
+    private void RedLight()
+    {
+
+    }
+
+    private void GreenLight()
+    {
+
     }
 }
