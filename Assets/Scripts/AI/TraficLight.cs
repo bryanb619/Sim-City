0e77ab4c4f2e41ba26a96f14a4a494396b37caa5
Steven Hall <stevenbryanb.work@gmail.com>
2024-04-30 23:23:22 +0100

    Add Traffic transtions & comments

--- Assets/Scripts/AI/TraficLight.cs
+++ Assets/Scripts/AI/TraficLight.cs
@@ -1,18 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using LibGameAI.FSMs;
+using System;
 
 
-public class TraficLight : MonoBehaviour
+public class TraficLight : MonoBehaviour, ITrafficLight
 {
     
-
     private StateMachine _fsm;
 
-    [SerializeField] private enum lightState
+
+
+    // -------------------- TEMP/ TEST ----------------------------------------
+    public enum lightState
     {
         yellow, red, green
     }
+    private lightState _currentLightState;
+    //--------------------------------------------------------------------------
+
+
 
     /// <summary>
     /// Start is called before the first frame update
@@ -22,6 +29,10 @@ public class TraficLight : MonoBehaviour
         System.Random rand = new System.Random(); 
 
         int i = rand.Next(0, 2);
+
+
+        // -------------------- States ----------------------------------------
+
 
         State yellowState = new State(  "Yellow", YellowLight,  null, null);
         State redState = new State(     "Red",    RedLight,     null, null);
@@ -33,12 +44,23 @@ public class TraficLight : MonoBehaviour
             yellowState, redState, greenState
         };
 
+
         _fsm = new StateMachine(states[i]);
 
 
-        // Transitions
+        // -------------------- Transitions ------------------------------------
 
-        
+        // change every 10 seconds
+        //() => Time.time > 5, null, lightState
+
+        greenState.AddTransition(new Transition(
+            () => _currentLightState == lightState.yellow, null, yellowState));
+
+        yellowState.AddTransition(new Transition(
+            () => _currentLightState == lightState.red, null, redState));
+
+        redState.AddTransition(new Transition(
+            () => _currentLightState == lightState.green, null, greenState));
     }
 
 
@@ -47,7 +69,8 @@ public class TraficLight : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        
+        Action actions = _fsm.Update();
+        actions?.Invoke();
     }
 
 
