450533ea489733cf59dd59818e33fc00d8911774
Steven Hall <stevenbryanb.work@gmail.com>
2024-05-06 18:22:02 +0100

    Add NavAgent & udpate pos with array

---
+++ Assets/Scripts/AI/NavAgentBehaviour.cs
@@ -0,0 +1,45 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GameAIPrototypes.SimpleNavMesh
+{
+    public class NavAgentBehaviour : MonoBehaviour
+    {
+        // Current goal of navigation agent
+        [SerializeField] private Transform[] goal;
+
+        // Reference to the NavMeshAgent component
+        private NavMeshAgent agent;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            // Get reference to the NavMeshAgent component
+            agent = GetComponent<NavMeshAgent>();
+            // Set initial agent goal
+            //agent.SetDestination(goal.position[]);
+        }
+
+        // Method called when agent collides with something
+        private void OnTriggerEnter(Collider other)
+        {
+            // Did agent collide with goal?
+            if (other.name == "Goal")
+                // If so, update destination (let goal reposition itself first)
+                Invoke("UpdateDestination", 0.1f);
+        }
+
+        // Update destination
+        private void UpdateDestination(int pos)
+        {
+            // Set destination to current goal position
+            agent.SetDestination(goal[pos].position);
+        }
+    }
+}
