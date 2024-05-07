2ff8a4507cdbb19fa567fb1d748479cb8e899e92
DiogoMeiraFreire <didi9934@gmail.com>
2024-05-07 18:35:19 +0100

    Addition of missing transitions in crazy

--- Assets/Scripts/AI/NavAgentBehaviour.cs
+++ Assets/Scripts/AI/NavAgentBehaviour.cs
@@ -1,11 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.AI;
-using LibGameAI.DecisionTrees;
+using LibGameAI.FSMs;
+using Random = UnityEngine.Random;
+
+
 
 namespace Assets.Scripts.AI
 {
-    public class NavAgentBehaviour : MonoBehaviour
-    {
+    // Automatically add the NavMeshAgent to the GameObject
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class NavAgentBehaviour : MonoBehaviour, IAgent
+    {   
+        // ---------------- interface ------------------------------------------
+
+        public AgentState State { get; private set; }
+        // ---------------------------------------------------------------------
+
         // Current goal of navigation agent
         [SerializeField] private GameObject[] goal;
 
@@ -13,10 +25,18 @@ namespace Assets.Scripts.AI
         private NavMeshAgent agent;
 
 
+        // ------------- FSM ---------------------------------------------------
+
+        private StateMachine _fsm;
+
+        private Color _color;
+
+        private MeshRenderer mesh;
+
+        //----------------------------------------------------------------------
+
 
         // The root of the decision tree
-        private IDecisionTreeNode root;
-
 
         // 
         private void Awake()
@@ -29,29 +49,77 @@ namespace Assets.Scripts.AI
             // Get reference to the NavMeshAgent component
             agent = GetComponent<NavMeshAgent>();
 
+            mesh = GetComponent<MeshRenderer>();
         }
 
 
         // Start is called before the first frame update
         private void Start()
-        {
+        {   
+
+            // --------------------- STATES ------------------------------------
+            State IdleState     = new State("Iddle",Idle, null, null);
+            State MovingState   = new State("Moving",Move, null, null);
+            State AccidentState = new State("Accident",Accident, null, null);
+            State CrazyState    = new State("Crazy", Crazy, null, null);
+
+            // ------------------- Color ---------------------------------------
+             _color = mesh.material.color;
+           
+
+            // ------------------------ TRANSITIONS ----------------------------
+
+
+            // IDLE => Move
+            IdleState.AddTransition(new Transition(
+                () => State == AgentState.Move,
+                null, MovingState));
+
+
+            // ----------------------------------------
+
+            // Move => Idle
+            MovingState.AddTransition(new Transition(
+                () => State == AgentState.Idle,
+                null, IdleState));
+
+            // Move => Crazy
+            MovingState.AddTransition(new Transition(
+                () => State == AgentState.Crazy,
+                null, CrazyState));
+
+            // Move => Accident
+            MovingState.AddTransition(new Transition(
+                () => State == AgentState.Idle,
+                null, IdleState));
+
+            // ---------------------------------------
+
+            // Crazy => Accident
+            CrazyState.AddTransition(new Transition(
+                () => State == AgentState.Accident,
+                null, AccidentState));
+
+            // Crazy => Move
+            CrazyState.AddTransition(new Transition(
+                () => State == AgentState.Move,
+                null, MovingState));
+
+            // Crazy => Idle
+            CrazyState.AddTransition(new Transition(
+                () => State == AgentState.Idle,
+                null, MovingState));
+
+
+            _fsm = new StateMachine(MovingState);
+            //------------------------------------------------------------------
+
             
             // Set initial agent goal
-            agent.SetDestination(goal[Random.Range(0, goal.Length)].transform.position);
+            agent.SetDestination(
+                goal[Random.Range(0, goal.Length)].transform.position);
 
 
-            RandomDecisionBehaviour rdb = new RandomDecisionBehaviour(
-            () => Random.value,() => Time.time, 25);
-
-
-            // stand action
-            IDecisionTreeNode standStill = new ActionNode(Stand);
-
-            // Move to target action
-            IDecisionTreeNode GoToNextPont = new ActionNode(Move);
-
-                                                        // if                   // true, false
-            IDecisionTreeNode nodeA = new DecisionNode(rdb.RandomDecision,  GoToNextPont,  standStill); 
         }
 
 
@@ -59,30 +127,22 @@ namespace Assets.Scripts.AI
         // Run the decision tree and execute the returned action
         private void Update()
         {
-            // /////////////////////// //
-            // Run decision tree here! //
-            // /////////////////////// //
 
-            ActionNode actionNode = root.MakeDecision() as ActionNode;
-            //if(ActionNode)
+            //----------------- Update FSM -------------------------------------
+            Action actions = _fsm.Update();
+            actions?.Invoke();
 
-            actionNode.Execute();
-
-            //(root.MakeDecision() as ActionNode).Execute();
-
+            //------------------------------------------------------------------
+         
         }
 
 
         // Method called when agent collides with something
         private void OnTriggerEnter(Collider other)
-        {
-           
-            // Did agent collide with goal?
-            if (other.name == "Goal")
-                // If so, update destination (let goal reposition itself first)
-
-                // MUST ADD INT POS
-                Invoke("Move", 0.1f);
+        { 
+            
+            if (other.tag == "CarDest")
+                Invoke("Idle", 0.1f);
         }
 
 
@@ -90,20 +150,73 @@ namespace Assets.Scripts.AI
 
         // ----------------------- ACTIONS -------------------------------------
 
-        private void Stand()
+#region  Idle & Move
+        private void Idle()
         {
-            // Move towards player
-            //MoveTowardsTarget(player.transform.position);
+            SetAgentMovement(true);
+            mesh.enabled = false;
+            agent.enabled = false;
         }
 
         private void Move()
         {
+            SetAgentMovement(false);
+            mesh.enabled = true;
+            agent.enabled = true;
 
             int pos = Random.Range(0, goal.Length);
 
             UpdateDestination(pos);
             
         }
+
+#endregion
+
+#region  Crazy
+        private void Crazy()
+        {
+          
+            float time = Random.Range(10F, 60F);
+            StartCoroutine(HitFlash(time));
+
+        }
+
+        // receive time of flash 
+        private IEnumerator HitFlash(float time)
+        {
+            int timer = 0; 
+
+            while (true)
+            {
+                timer ++;
+
+
+                if(timer< time)
+                {
+                    mesh.material.color = Color.yellow;
+                    yield return new WaitForSeconds(0.2f);
+                    mesh.material.color = _color;
+                }
+              
+            }
+            
+        }
+
+#endregion
+
+
+#region Accident Actions
+        private void Accident()
+        {
+            SetAgentMovement(true);
+            mesh.material.color = Color.red;
+        
+        }
+
+      
+#endregion
+
+        // ---------------------------------------------------------------------
 
 
         // Update destination
@@ -112,5 +225,21 @@ namespace Assets.Scripts.AI
             // Set destination to current goal position
             agent.SetDestination(goal[pos].transform.position);
         }
+
+
+        private void SetAgentMovement(bool stop)
+        {
+            if(stop)
+            {
+                agent.speed = 0; 
+            }
+            else
+            {   
+                // TODO ADD A default speed
+                agent.speed = 3.5f;
+            }
+
+        }
     }
+
 }
