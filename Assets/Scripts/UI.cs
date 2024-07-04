66d394a210cbaa9478588fd8b2b3dabc40e21b10
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-04 16:54:44 +0100

    Add Properties, some XML & StartSim (UI class)

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,62 +7,98 @@ namespace Assets.Scripts.AI
 {
     public class UI : MonoBehaviour
     {
-        // Reference to the UI GameObject
 
-        [Tooltip("Ref to UI. Can be null. If Null it will automatically " 
-        +"find the first Image component in the children.")]
+#region Variables
+
+        // Reference to the UI GameObject
         [SerializeField] private GameObject      _ui;
 
-        // Key to toggle the UI on/off
-        [Tooltip("Key to toggle the UI on/off")]
-        [SerializeField] private KeyCode         _key;
+        [SerializeField] private GameObject     _aiDirector;
 
+        private AIDirector aIDirector;
 
-        [SerializeField] private Slider         _maxCarSlider;
-        [SerializeField] private Slider         _maxPedSlider;
-        [SerializeField] private Slider         _maxCarTimeStoped;
-        [SerializeField] private Slider         _maxPedTimeStopped;
+        //  UI Sliders
+        //Stores value of user set value of vehicles.
+        [SerializeField] private Slider         _carSlider;
+        [SerializeField] private Slider         _pedSlider;
+        [SerializeField] private Slider         _carTimeStoped;
+        [SerializeField] private Slider         _pedTimeStopped;
         [SerializeField] private Slider         _maxTimeInAccident;
         [SerializeField] private Slider         _maxTimeInCrazy;
 
+#endregion
 
+#region Properties
 
         /// <summary>
-        /// Start is called before the first frame update.
-        /// If _ui is null, Start will automatically find the first Image component in the children.
+        /// Read-only property reads the value of _maxCarSlider.
+        /// Automatically converts slider.value to int.
+        /// </summary>
+        /// <value>Returns value of the maximum of cars slider.</value>
+        public int MaxCars { 
+            get => Convert.ToInt32(_carSlider.value);}
+
+        /// <summary>
+        /// Read-only property reads the value of _pedSlider
+        /// </summary>
+        /// <value>Returns value of the maximum of cars slider.</value>
+        public int MaxPeds { 
+            get => Convert.ToInt32(_pedSlider.value);}
+
+        /// <summary>
+        /// Read-only property reads the value of _carTimeStoped slider
+        /// </summary>
+        /// <value>Returns value of the maximum of cars slider.</value>
+        public int CarStoppedTime { 
+            get => Convert.ToInt32(_carTimeStoped.value);}
+
+        /// <summary>
+        /// Read-only property reads the value of _pedTimeStopped slider
+        /// </summary>
+        /// <value>Returns value of the maximum of cars slider.</value>
+        public int PedStoppedTime { 
+            get => Convert.ToInt32(_pedTimeStopped.value);}
+
+        /// <summary>
+        /// Read-only property reads the value of _maxTimeInAccident slider
+        /// </summary>
+        /// <value>Returns value of the maximum of cars slider.</value>
+        public int MaxTimeInAccident { 
+            get => Convert.ToInt32(_maxTimeInAccident.value);}
+
+        /// <summary>
+        /// Read-only property reads the value of _maxTimeInCrazy slider
+        /// </summary>
+        /// <value>Returns value of _maxTimeInCrazy slider.</value>
+        public int MaxTimeInCrazy { 
+            get => Convert.ToInt32(_maxTimeInCrazy.value);}
+
+#endregion
+
+        /// <summary>
+        /// Start is called before the first frame update
         /// </summary>
         private void Start()
-        {
-            _ui =  GetComponentInChildren<Image>().gameObject;
+        {   
+            _ui.SetActive(true);
+
+            // get AIDirector component and pass it to aIDirector
+            aIDirector = _aiDirector.GetComponent<AIDirector>();
         }
 
-
         /// <summary>
-        /// Update is called once per frame
-        /// Detects if the key is pressed and toggles the UI on/off.
+        /// 
         /// </summary>
-        private void Update()
+        public void StartSim()
         {
-            if (Input.GetKeyDown(_key))
+            _ui.SetActive(false);
+            _aiDirector.SetActive(true);
+            
+            if(!aIDirector.SimStar)
             {
-                DetectClick();
+                
             }
-        }
 
-
-        /// <summary>
-        /// Public method can be used as a button or key press to toggle the UI on/off.
-        /// </summary>
-        public void DetectClick()
-        {
-            if (_ui.activeSelf)
-            {
-                _ui.SetActive(false);
-            }
-            else
-            {
-                _ui.SetActive(true);
-            }
         }
     }
 }
