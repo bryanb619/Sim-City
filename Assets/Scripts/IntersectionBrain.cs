dfa3bc5b569cd88a806dfa5155a7b2415186a557
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-05 18:54:19 +0100

    Add intersectionBrain class

---
+++ Assets/Scripts/IntersectionBrain.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class IntersectionBrain : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+        
+    }
+}
