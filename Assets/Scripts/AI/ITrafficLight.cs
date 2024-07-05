e43b6ae876fbba6ae6b94b008e9267bd2cccbf87
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-05 23:38:18 +0100

    Add SwapLightState (ITrafficLight)

--- Assets/Scripts/AI/ITrafficLight.cs
+++ Assets/Scripts/AI/ITrafficLight.cs
@@ -1,9 +1,14 @@
 using Assets.Scripts.AI;
+using LibGameAI.FSMs;
 
 /// <summary>
 /// 
 /// </summary>
 public interface ITrafficLight 
 {
+
     LightState Light { get; }
+
+    void SwapLightState();
+
 }
