880036b5ddb270660da78a3b0c1bde0c1e499622
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-05 19:42:51 +0100

    Add List & editor header (IntersectionBrain)

--- Assets/Scripts/IntersectionBrain.cs
+++ Assets/Scripts/IntersectionBrain.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class IntersectionBrain : MonoBehaviour
-{
+{   
+
+    [Header("List of control points for the intersection\n" +
+    "Element 0: Up\nElement 1: Down\nElement 2: Left\nElement 3: Right")]
+    [SerializeField]
+    private List<GameObject> controlPoints = new List<GameObject>();
     /// <summary>
     /// Start is called before the first frame update
     /// </summary>
     private void Start()
     {
-        
+
     }
 
     /// <summary>
