65d0b75f86301f4f6c63d99f0847fc4dce77897c
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-07 15:27:41 +0100

    Remove StartUI Sliders

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,107 +1,141 @@
 using System;
+using Assets.Scripts.AI;
 using UnityEngine;
-using UnityEngine.UI;
 
 
-namespace Assets.Scripts.AI
+public class UI : MonoBehaviour
 {
-    public class UI : MonoBehaviour
-    {
 
 #region Variables
 
-        // Reference to the UI GameObject
-        [SerializeField] private GameObject      _ui;
+    // Reference to the UI GameObject
+    [SerializeField] private GameObject      _startUI;
 
-        [SerializeField] private GameObject     _aiDirector;
+    [SerializeField] private GameObject      _simUI;
 
-        private AIDirector aIDirector;
+    [SerializeField] private GameObject     _aiDirector;
 
-        //  UI Sliders
-        //Stores value of user set value of vehicles.
-        [SerializeField] private Slider         _carSlider;
-        [SerializeField] private Slider         _pedSlider;
-        [SerializeField] private Slider         _carTimeStoped;
-        [SerializeField] private Slider         _pedTimeStopped;
-        [SerializeField] private Slider         _maxTimeInAccident;
-        [SerializeField] private Slider         _maxTimeInCrazy;
+    private AIDirector aIDirector;
+
+
+    [Range(0, 50)]
+    [SerializeField] private int        _carSlider = 25;
+
+    [Range(0, 75)]
+    [SerializeField] private int         _pedSlider = 30;
+
+    [Range(0, 75)]
+    [SerializeField] private int        _carTimeStoped = 10;
+
+    [Range(0, 75)]
+    [SerializeField] private int         _pedTimeStopped = 10;
+
+    [Range(0, 75)]
+    [SerializeField] private int         _maxTimeInAccident = 15;
+
+    [Range(0, 75)]
+    [SerializeField] private int         _maxTimeInCrazy = 30;
+
 
 #endregion
 
 #region Properties
 
-        /// <summary>
-        /// Read-only property reads the value of _maxCarSlider.
-        /// Automatically converts slider.value to int.
-        /// </summary>
-        /// <value>Returns value of the maximum of cars slider.</value>
-        public int MaxCars { 
-            get => Convert.ToInt32(_carSlider.value);}
+    /// <summary>
+    /// Read-only property reads the value of _maxCarSlider.
+    /// Automatically converts slider.value to int.
+    /// </summary>
+    /// <value>Returns value of the maximum of cars slider.</value>
+    public int MaxCars { 
+        get => _carSlider;}
 
-        /// <summary>
-        /// Read-only property reads the value of _pedSlider
-        /// </summary>
-        /// <value>Returns value of the maximum of cars slider.</value>
-        public int MaxPeds { 
-            get => Convert.ToInt32(_pedSlider.value);}
+    /// <summary>
+    /// Read-only property reads the value of _pedSlider
+    /// </summary>
+    /// <value>Returns value of the maximum of cars slider.</value>
+    public int MaxPeds { 
+        get => _pedSlider;}
 
-        /// <summary>
-        /// Read-only property reads the value of _carTimeStoped slider
-        /// </summary>
-        /// <value>Returns value of the maximum of cars slider.</value>
-        public int CarStoppedTime { 
-            get => Convert.ToInt32(_carTimeStoped.value);}
+    /// <summary>
+    /// Read-only property reads the value of _carTimeStoped slider
+    /// </summary>
+    /// <value>Returns value of the maximum of peds slider.</value>
+    public int CarStoppedTime { 
+        get => _carTimeStoped;}
 
-        /// <summary>
-        /// Read-only property reads the value of _pedTimeStopped slider
-        /// </summary>
-        /// <value>Returns value of the maximum of cars slider.</value>
-        public int PedStoppedTime { 
-            get => Convert.ToInt32(_pedTimeStopped.value);}
+    /// <summary>
+    /// Read-only property reads the value of _pedTimeStopped slider
+    /// </summary>
+    /// <value>Returns value of the maximum of cars slider.</value>
+    public int PedStoppedTime { 
+        get => _pedTimeStopped;}
 
-        /// <summary>
-        /// Read-only property reads the value of _maxTimeInAccident slider
-        /// </summary>
-        /// <value>Returns value of the maximum of cars slider.</value>
-        public int MaxTimeInAccident { 
-            get => Convert.ToInt32(_maxTimeInAccident.value);}
+    /// <summary>
+    /// Read-only property reads the value of _maxTimeInAccident slider
+    /// </summary>
+    /// <value>Returns value of the maximum of cars slider.</value>
+    public int MaxTimeInAccident { 
+        get => _maxTimeInAccident;}
 
-        /// <summary>
-        /// Read-only property reads the value of _maxTimeInCrazy slider
-        /// </summary>
-        /// <value>Returns value of _maxTimeInCrazy slider.</value>
-        public int MaxTimeInCrazy { 
-            get => Convert.ToInt32(_maxTimeInCrazy.value);}
+    /// <summary>
+    /// Read-only property reads the value of _maxTimeInCrazy slider
+    /// </summary>
+    /// <value>Returns value of _maxTimeInCrazy slider.</value>
+    public int MaxTimeInCrazy { 
+        get => _maxTimeInCrazy;}
 
 #endregion
 
-        /// <summary>
-        /// Start is called before the first frame update
-        /// </summary>
-        private void Awake()
-        {   
-            _ui.SetActive(true);
-            _aiDirector.SetActive(false);
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    private void Awake()
+    {   
+        //_startUI.SetActive(true);
+        //_simUI.SetActive(false);
+        _aiDirector.SetActive(false);
 
-            // get AIDirector component and pass it to aIDirector
-            aIDirector = _aiDirector.GetComponent<AIDirector>();
-        }
+        // get AIDirector component and pass it to aIDirector
+        aIDirector = _aiDirector.GetComponent<AIDirector>();
+    }
 
         /// <summary>
-        /// 
-        /// </summary>
-        public void StartSim()
+    /// 
+    /// </summary>
+    public void StartSim()
+    {
+        if(!aIDirector.SimStar)
         {
-            _ui.SetActive(false);
             _aiDirector.SetActive(true);
-
-            if(!aIDirector.SimStar)
-            {
-                
-            }
-
+            _simUI.SetActive(true);
         }
+    }
 
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+        ChangeUIState();
+    }
+
+
+    private void ChangeUIState()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+                switch(_simUI.activeSelf)
+            {
+                case true:
+                    _simUI.SetActive(false);
+                    break;
+                case false:
+                    _simUI.SetActive(true);
+                    break;
+            }
+            
+        }
     }
 }
+
 // -----------------------------------------------------------------------------
