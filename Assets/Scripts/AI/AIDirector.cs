058dc51499c152a8141a60f1e40c878e5a4240ce
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-13 12:15:18 +0100

    Add XML (AIDirector)

--- Assets/Scripts/AI/AIDirector.cs
+++ Assets/Scripts/AI/AIDirector.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 namespace Assets.Scripts.AI
 {
     /// <summary>
-    /// 
+    /// Represents the director of AI in the simulation.
+    /// AI Director is responsable setting the parameters of agents
+    /// instantiate of agents, adding to their respective list (car or ped list)
+    /// while also implementing the logic for the chaos button.
     /// </summary>
     public class AIDirector : MonoBehaviour
     {
@@ -56,17 +59,21 @@ namespace Assets.Scripts.AI
 
         // list of agents
 
-        // car list
+        /// <summary>
+        /// Represents the car list of game objects
+        /// </summary>
         private List<GameObject> _carList = new List<GameObject>();
 
-        // ped list
+        /// <summary>
+        /// Represents the Ped list of game objects
+        /// </summary>
         private List<GameObject> _pedList = new List<GameObject>();
 
+        [SerializeField] 
+        private UI _ui;
 
-        [SerializeField] private UI _ui;
-
-
-        [SerializeField] private bool doubleTimeScale;
+        [SerializeField] 
+        private bool doubleTimeScale;
 
         /// <summary>
         /// Start is called before the first frame update
@@ -83,10 +90,13 @@ namespace Assets.Scripts.AI
         }
 
         /// <summary>
-        /// 
+        /// This IEnumerator spawns agents into the simulation while also 
+        /// adding them to their respective list (car or ped).
         /// </summary>
-        /// <param name="objAI"></param>
-        /// <param name="quantity"></param>
+        /// <param name="objAI">game object to be spawned</param>
+        /// <param name="quantity">
+        /// quantity of previous gameObject (objAI) to be spawned in sim
+        /// </param>
         private IEnumerator SpawnAgents(GameObject objAI, int quantity)
         {
             int rp;
@@ -148,14 +158,21 @@ namespace Assets.Scripts.AI
 
         }
 
-        private IEnumerator WaitTime(float seconds)
-        {
-            yield return new WaitForSeconds(seconds);
-        }
-
         /// <summary>
-        /// 
+        /// Method attempts to sets a random agent in chaos mode.
+        /// bool value must be set so the method can correctly set an agent 
+        /// to chaos mode.
+        /// Pass a true value and method will use the car list to randomnly 
+        /// chose a vehicle.
+        /// Pass a false value and method will use the ped list to radomnly
+        /// chose a ped.
+        /// When no agent is available to be ped, a debug message will appear in
+        /// the Unity console.
         /// </summary>
+        /// <param name="isVehicle">
+        /// True: this agent is of type vehicle
+        /// False: this is not of type vehicle (is a ped)
+        /// </param>
         public void SelectChaosMode(bool isVehicle)
         {
             Agent agent;
@@ -164,14 +181,14 @@ namespace Assets.Scripts.AI
 
             bool foundAgent = false;
 
-            string unit = "";
+            string unit;
 
             List<GameObject> units; 
 
 
             do
             {
-                
+                // check if is vehicle
                 if (isVehicle)
                 {
                     unit = "Car";
@@ -179,60 +196,45 @@ namespace Assets.Scripts.AI
 
                 }
 
+                // else is a pedestrian
                 else 
                 {
                     unit = "Ped";
                     units = _pedList;
                 }
 
-
+                // obtain a random value withing the list count
                 r = Random.Range(0, units.Count);
 
-
+                // obtain the agent class
                 agent = units[r].GetComponent<Agent>();
 
-
-
+                // is this agent a chaos candidate?
                 if (agent.CanBeChaos())
-                {
+                {   
+                    // found a agent & break
                     foundAgent = true;
                     break;
                 }
 
+                // agent not valid, so increase attempts!
                 attempts++;
-            
             }
 
+            // loop until attemps does not pass the list count
             while (attempts < units.Count);
 
+            // use this agent
             if (foundAgent) 
-            { 
+            {   
+                // set to chaos
                 agent.SetChaosAgent();
                 print($"{unit} {r}: Chaos mode");
             }
 
+            // no viable agent found, display message to user
             else Debug.Log($"No {unit} available to be chaos");
             
-        }
-
-        public void SelectPedChaosMode()
-        {
-
-            int i = 0;
-
-            i = Random.Range(0, _pedList.Count);
-
-            _pedList[i].GetComponent<NavAgentBehaviour>();
-
-            /*
-            int i = Random.Range(0, _agents.Length);
-
-
-            if(navAgent[i].State == AgentState.Move)
-            {
-                navAgent[i].State = AgentState.Chaos;
-            }
-            */
         }
     }
 }
