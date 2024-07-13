8aad3610d313228fa10f7d61ac35d3e36362da0a
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-13 12:40:02 +0100

    Add XML (LightState)

--- Assets/Scripts/AI/LightState.cs
+++ Assets/Scripts/AI/LightState.cs
@@ -1,8 +1,19 @@
 namespace Assets.Scripts.AI
-{
+{   
+    /// <summary>
+    /// Enum that represents the state of the traffic light
+    /// </summary>
     public enum LightState
     {
-        green, red
+        /// <summary>
+        /// Represents the state of the traffic light when it is green.
+        /// </summary>
+        green, 
+        
+        /// <summary>
+        /// Represents the state of the traffic light when it is red.
+        /// </summary>
+        red
     }
 
 }
