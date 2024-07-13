91b18ab18db7006beaefbcf793b8c9a9feae0309
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-13 12:42:01 +0100

    Add XML (AgentState)

--- Assets/Scripts/AI/AgentState.cs
+++ Assets/Scripts/AI/AgentState.cs
@@ -1,8 +1,25 @@
 namespace Assets.Scripts.AI
-{
+{   
+
+    /// <summary>
+    /// Reprsents the agent state in the simulation.
+    /// </summary>
     public enum AgentState 
     {
-        Move, Idle, Accident
+        /// <summary>
+        /// State of the agent is proceding to destination.
+        /// </summary>
+        Move, 
+
+        /// <summary>
+        /// State of the agent when it is stopped at a destination.
+        /// </summary>
+        Idle, 
+
+        /// <summary>
+        /// State of the agent when it is in an accident.
+        /// </summary>
+        Accident
     }
 }
 
