f6033df264ef592f9fc23149acaa926f719e8480
Steven Hall <stevenbryanb.work@gmail.com>
2024-07-13 12:28:49 +0100

    Add XML (Intersection Brain)

--- Assets/Scripts/AI/IntersectionBrain.cs
+++ Assets/Scripts/AI/IntersectionBrain.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 namespace Assets.Scripts.AI
 {
     /// <summary>
-    /// 
+    /// Represents a class responsable for handling Traffic light intersections
+    /// in the simulation.
+    /// Intersection Brain is also reponsible for setting the timer for the swap 
+    /// of the traffic lights state.
     /// </summary>
     public class IntersectionBrain : MonoBehaviour
     {
@@ -19,28 +22,28 @@ namespace Assets.Scripts.AI
         "Traffic light: Right")]
 
         [Tooltip("This is a list this intersection's traffic lights.")]
-
         [SerializeField]
-        private List<TrafficLight> controlPoints = new List<TrafficLight>();
-
 
         /// <summary>
-        /// Start is called before the first frame update
+        /// Represents a list of traffic lights for the current intersection.
+        /// </summary>
+        /// <typeparam name="TrafficLight">Traffic light component</typeparam>
+        /// <returns>returns a list of Traffic Light components</returns>
+        private List<TrafficLight> controlPoints = new List<TrafficLight>();
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// Calls the method to check if the traffic lights are properly configured.
         /// </summary>
         private void Awake()
         {
             CheckLightConfig();
         }
-
+        
         /// <summary>
-        /// Start is called before the first frame update
+        /// Method checks if the traffic lights are properly configured
+        /// if not configured, it will throw an error to the unity console.
         /// </summary>
-        private void Start()
-        {
-            StartCoroutine(UpdateLightState());
-        }
-
-
         private void CheckLightConfig()
         {
             if (controlPoints.Count == 0 || controlPoints == null)
@@ -49,14 +52,20 @@ namespace Assets.Scripts.AI
             }
         }
 
+        /// <summary>
+        /// Start is called before the first frame update
+        /// Calls the coroutine to update the light state
+        /// </summary>
+        private void Start()
+        {
+            StartCoroutine(UpdateLightState());
+        }
 
         /// <summary>
         /// Coroutine updates time, checks and changes the single states while 
-        /// also resetting the time
+        /// also resetting the time after the max time is reached.
         /// </summary>
-        /// <returns>
-        ///  waits for seconds
-        /// </returns>
+        /// <returns> waits for seconds.</returns>
         private IEnumerator UpdateLightState()
         {
             int time = 0;
@@ -65,16 +74,18 @@ namespace Assets.Scripts.AI
             {
                 time++;
 
+                // check if time is greater than max time
                 if (time >= lightMaxTime)
                 {
-
+                    
+                    // swap the light state for each traffic light in the list
                     foreach (TrafficLight trafficLight in controlPoints)
                     {
-
+                        // check if traffic light is not null
                         if(trafficLight != null)
-                        {
+                        {   
+                            // swap the light state
                             trafficLight.SwapLightState();
-                            
                         }
 
                     }
@@ -83,10 +94,8 @@ namespace Assets.Scripts.AI
                     time = 0;
                 }
 
-#if UNITY_EDITOR // DEBUG TIME
-
+                // DEBUG TIME
                 //print(time);
-#endif
 
                 // wait 1 second
                 yield return new WaitForSeconds(1);
